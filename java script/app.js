6134d2a63576bdcb9e32854b3da012bdba33110e
Alia Shrief <aliashrief90@gmail.com>
2023-08-06 20:18:30 +0300

    Update app.js

--- java script/app.js
+++ java script/app.js
@@ -10,25 +10,25 @@
 			$(".mobile-navigation").slideToggle();
 		});
 
-		var map = $(".map");
-		var latitude = map.data("latitude");
-		var longitude = map.data("longitude");
-		if( map.length ){
+		// var map = $(".map");
+		// var latitude = map.data("latitude");
+		// var longitude = map.data("longitude");
+		// if( map.length ){
 			
-			map.gmap3({
-				map:{
-					options:{
-						center: [latitude,longitude],
-						zoom: 15,
-						scrollwheel: false
-					}
-				},
-				marker:{
-					latLng: [latitude,longitude],
-				}
-			});
+		// 	map.gmap3({
+		// 		map:{
+		// 			options:{
+		// 				center: [latitude,longitude],
+		// 				zoom: 15,
+		// 				scrollwheel: false
+		// 			}
+		// 		},
+		// 		marker:{
+		// 			latLng: [latitude,longitude],
+		// 		}
+		// 	});
 			
-		}
+		// }
 	});
 
 	$(window).load(function(){
